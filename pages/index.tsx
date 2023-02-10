ad3668bcef40e0984cb629792fe655bcdfe68a02
Nicolas Tourne <nicotourne@gmail.com>
2023-02-10 11:49:54 -0300

    feat: Pass props to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import DisplayData from "@/src/components/DisplayData";
+import { GetServerSideProps } from "next/types";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type Props = {
+  createdAt: string;
+  name: string;
+};
+export default function Home({ createdAt, name }: Props) {
   return (
     <>
       <Head>
@@ -16,9 +21,40 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          The test is being done with these values:
+          <ul>
+            <li>name: {`"${name}"`}</li>
+            <li>last time updated: {`"${createdAt}"`}</li>
+          </ul>
+        </div>
+        <div className={styles.description}>
           <DisplayData />
         </div>
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=900, stale-while-revalidate=21600"
+  );
+
+  const name = "home";
+
+  const createdAt = await fetch(
+    `https://nextjs-api-experiments.vercel.app/api/test?name=${name}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      return data.createdAt;
+    });
+
+  return {
+    props: {
+      createdAt,
+      name,
+    },
+  };
+};
